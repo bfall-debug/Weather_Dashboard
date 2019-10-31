af7e4229cd0caea30a30cda7abb1f6735ae9965d
BFall-Debug <bfall92@gmail.com>
2019-10-30 22:20:46 -0400

    javascript polished

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,148 @@
-var cities = ["Austin","Chicago","New York","Orlando","San Francisco","Seattle","Denver","Atlanta"];
+var apiKey = "9612cbacdd5ba9a49362661b9ca9dfec";
+var CurrentDate = moment();
+
+
+var cities = JSON.parse(localStorage.getItem('cities'));
+if (cities === null){
+    cities = []
+}
 
 cities.forEach(function(city){
-    NewSearch(city);
+    SearchHistory(city);
 })
 
+
 $(".btn").click(function(){
+
     var textbox = $(this).parent().siblings("#textbox")
     var search = textbox.val()
+
+    if (search == "" || search == null){
+        return;
+    }
+
     textbox.val("");
+    cities.push(search)
+    localStorage.setItem("cities", JSON.stringify(cities));
 
     NewSearch(search);
+    SearchHistory(search);
+
+
+});
+
+$(".city").click(function(){
+    alert($(this).text());
+    NewSearch($(this).text())
 });
 
 function NewSearch(citySearch){
-    var city = $("<div>");
-    city.addClass("col-12 city py-2");
-    city.text(citySearch)
-    $(".left-container").append(city);
-
-    
-
     
 
     var header = $("<h3>");
-        header.text(citySearch + " (8/15/2019)");
+        header.text(citySearch + " (" + CurrentDate.format('L') + ")");
     var temperature = $("<p>");
-        temperature.text("Temperature: ");
     var humidity = $("<p>");
-        humidity.text("Humidity: ");
     var windSpeed = $("<p>");
-        windSpeed.text("Wind Speed: ");
     var UV = $("<p>");
-        UV.text("UV Index: ");
+    UV.text("UV Index:  ")
+
+
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&APPID=${apiKey}&units=imperial`;
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function(response) {
+        var temp = response.main.temp
+        temperature.text("Temperature: " + temp + String.fromCharCode(176) + "F");
+        humidity.text("Humidity: " + response.main.humidity + "%")
+        windSpeed.text("Wind Speed: " + response.wind.speed + " MPH");
+
+        var icon = $("<img>");
+        var iconcode = response.weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        icon.attr("src",iconurl)
+        
+        header.append(icon);
+
+        var lon = response.coord.lon;
+        var lat = response.coord.lat
+
+        var indexUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&APPID=${apiKey}`
+
+        $.ajax({
+            url: indexUrl,
+            method: "GET"
+        }).then(function(response) {
+            var UvText = $("<span>");
+            UvText.text(response.value)
+            UvText.addClass("UV");
+            UV.append(UvText);
+        })
+
+    });
+
+    
 
     var details = $("<div>");
         details.addClass("details")
         details.append(header,temperature,humidity,windSpeed,UV);
-
     var forecastHeader = $("<h4>");
         forecastHeader.addClass("mt-3")
         forecastHeader.text("5-Day Forecast:")
     var days = $("<div>");
         days.addClass("row justify-content-between mx-1")
-    
+
+
+
+
+
+
+
+
+        var url = `https://api.openweathermap.org/data/2.5/forecast?q=${citySearch}&APPID=${apiKey}&units=imperial`;
+        $.ajax({
+            url: url,
+            method: "GET"
+        }).then(function(response) {
+            
+        
     for( i = 0; i < 5; i++ ){
         var day = $("<div>");
         day.addClass("col-2 day px-1 pt-2");
-
+        var CurrentDate = moment();
+        CurrentDate.add(i, 'days').format("L");
         var date = $("<h6>");
-        date.text("8/16/2019");
+        date.text(CurrentDate.format("L"));
+
         var icon = $("<img>");
+        var iconcode = response.list[i].weather[0].icon;
+        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        icon.attr("src",iconurl)
+
         var temp = $("<p>");
-        temp.text("Temp: ");
+        temp.text("Temp: " + response.list[i].main.temp +  String.fromCharCode(176) + "F");
         var humid = $("<p>");
-        humid.text("Humidity: ");
+        humid.text("Humidity: " + response.list[i].main.humidity + "%");
 
         day.append(date,icon,temp,humid);
         days.append(day)
     }
 
     var forecast = $("<div>");
-        forecast.append(forecastHeader, days)
-
+    forecast.append(forecastHeader, days)
 
     var rightContainer = $(".right-container");
     rightContainer.empty();
     rightContainer.append(details,forecast);
+
+    });
+}
+
+function SearchHistory(citySearch){
+    var city = $("<div>");
+    city.addClass("col-12 city py-2");
+    city.text(citySearch)
+    $(".left-container").append(city);
+    
 }
